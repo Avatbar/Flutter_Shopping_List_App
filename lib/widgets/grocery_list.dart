9b49b6ad493fc3a13c8a82af440060b00770e780
Avatbar <tadeasmachacek@gmail.com>
2024-01-09 00:21:49 +0100

    added function for remove items from shopping list

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../data/dummy_items.dart';
 import '../models/grocery_item.dart';
 import 'new_item.dart';
 
@@ -27,8 +26,55 @@ class _GroceryListState extends State<GroceryList> {
     }
   }
 
+  void _deleteItem(String id) {
+    setState(() {
+      _groceryItems.removeWhere((item) => item.id == id);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No items yet!'),
+    );
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+          itemCount: _groceryItems.length,
+          itemBuilder: (context, index) {
+            final item = _groceryItems[index];
+            return Dismissible(
+              key: ValueKey(item.id),
+              onDismissed: (direction) {
+                _deleteItem(item.id);
+              },
+              background: Container(
+                color: Theme.of(context).colorScheme.error,
+                alignment: Alignment.centerRight,
+                padding: const EdgeInsets.only(right: 20),
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 4,
+                ),
+                child: const Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: item.category.color,
+                ),
+                title: Text(item.name),
+                subtitle: Text(item.category.name),
+                trailing: Text('${item.quantity}x'),
+              ),
+            );
+          }
+      );
+    }
+
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Grocery List'),
@@ -39,20 +85,7 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ],
       ),
-      body: ListView.builder(
-          itemCount: _groceryItems.length,
-          itemBuilder: (context, index) {
-            final item = _groceryItems[index];
-            return ListTile(
-              leading: CircleAvatar(
-                backgroundColor: item.category.color,
-              ),
-              title: Text(item.name),
-              subtitle: Text(item.category.name),
-              trailing: Text('${item.quantity}x'),
-            );
-          }
-      ),
+      body: content,
     );
   }
 }
